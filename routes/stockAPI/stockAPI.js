fc744f419e174450412760fe67a6045540727b4f
Derek Irwin <derek.irwin@gmail.com>
2018-10-04 14:14:46 -0400

    Routing updates. Added missing front-end test files

--- routes/stockAPI/stockAPI.js
+++ routes/stockAPI/stockAPI.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
-const stockAPIControllers = require("../../controllers/stockAPIControllers");
-
-const router = require("express").Router();
 let request = require("request");
 let rp = require('request-promise');
-
+const stockAPIControllers = require("../../controllers/stockAPIControllers");
 
 // Matches with "/stockapi/"
+router
+    .route("/")
+    .get((req, res) => {
+        console.log(req);
+        return res.status(200).send({ success: true, msg: '/stockapi' });
+    });
 
 // This route will return the "latest" daily info for all of the user's stocks
 router
     .route("/getalllatest")
     .get((req, res) => {
         console.log("/getalllatest");
+        return res.status(200).send({ success: true, msg: '/getalllatest' });
     });
 
 // This route will return the historic info for all of the user's stocks
@@ -21,6 +25,7 @@ router
     .route("/getallhistoric/:startdate/:enddate")
     .get((req, res) => {
         console.log(`/getallhistoric/:startdate(${startdate})/:enddate(${enddate})`);
+        return res.status(200).send({ success: true, msg: `/getallhistoric/:startdate(${startdate})/:enddate(${enddate})` });
     });
 
 // This route will return the historic info for one single stock
@@ -29,6 +34,7 @@ router
     .route("/getonehistoric/:ticker")
     .get((req, res) => {
         console.log(`/getonehistoric/:ticker(${ticker})`);
+        return res.status(200).send({ success: true, msg: `/getonehistoric/:ticker(${ticker})` });
     });
 
 
